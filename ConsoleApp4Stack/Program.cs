61a6df23c10d81335208006bc73c66b1b99cdd72
oscarcortez <oscarkortez@gmail.com>
2021-03-31 18:43:30 -0400

    Add project files.

---
+++ ConsoleApp4Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Stack<string> numbers = new Stack<string>();
+            numbers.Push("one");
+            numbers.Push("two");
+            numbers.Push("three");
+            numbers.Push("four");
+            numbers.Push("five");
+
+            numbers.Pop();
+            Console.WriteLine(numbers.Peek());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number}");
+            }
+
+            
+        }
+    }
+}
